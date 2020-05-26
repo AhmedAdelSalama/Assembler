ad3fa64a6fd2c7a8a35ee78c7f2627fa4cce620d
EngAhmedWaleed <ahmedwaleed@csed22.com>
2020-05-26 05:53:45 +0200

    Fresh and Organized

---
+++ Assembler.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Assembler.cpp
+// Author      : https://youtu.be/AhCLgRVcKMg
+// Version     : 0.0v
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl; // prints Hello World
+	return 0;
+}
+
+//	Ctrl + B before running !!
