764df5ba783bd910f544c1f7944505bf51d71c54
bkhalf <bahaakhalf8@gmail.com>
2020-05-28 19:54:17 +0200

    Add files via upload

--- Instruction.h
+++ Instruction.h
@@ -7,6 +7,8 @@
 
 #include <string>
 #include "Expression.h"
+#include "SymbolTable.h"
+
 using namespace std;
 
 #ifndef INSTRUCTION_H_
@@ -16,16 +18,24 @@ class Instruction {
 public:
 	string label;
 	string operation;
-	Expression operand;   // e.g. for most Format 3 instructions it will be the TA,
+	string operator1;
+	string operand1 ;
+	string operand2;
+	int instructionLenght;
+	SymbolTable sym;
+	//Expression operand;   // e.g. for most Format 3 instructions it will be the TA,
 						// So you are required to calculate the disp.
 
 	// These should be 0's and 1's (instantiated as chars "just for readability")
 	//bool flags[] = { 'n', 'i', 'x', 'b', 'p', 'e' };
 
 	int LOCCTR;
-
-	Instruction(string _label, string _operation, Expression _operand, int _LOCCTR);
+//	Instruction(string _label, string _operation, Expression _operand, int _LOCCTR);
+    Instruction(string _label, string _operation, string _operand1,string _operator1,string _operand2,int _LOCCTR,SymbolTable _sym);
 	int getInstructionLength();
+	int expression();
+	string perform();
+
 };
 
 class StorageInstruction: Instruction {
